038fc2e711b2fee5b4d10233085ca54a84879775
Ejay Gabriel <eebudonihian@gmail.com>
2025-04-17 17:09:57 -0500

    Added KeyTech folder and it's assets

---
+++ KeyTech-Project/keyTech.js
@@ -0,0 +1,10 @@
+/* this is an alert message for keytech */
+alert("hello Welcome; project is the going on");
+// adding an event to the learn more button (LMB) in the landing page
+let learnMoreButton = document.querySelector('.lmb');
+learnMoreButton.addEventListener('click', ()=> {
+alert('Functionalities coming soon; stay tuned');
+})
+
+/* more features coming soon 
+TBD*/
