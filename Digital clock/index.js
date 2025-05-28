694a738ca2bdfdc2a05ef04e28988a1337b68e76
Ejay-DSKTP <eebudonihian@gmail.com>
2025-05-28 03:12:07 -0500

    feat: Add AM/PM display functionality to digital clock

---
+++ Digital clock/index.js
@@ -0,0 +1,44 @@
+const hoursEl = document.querySelector('.hours'), 
+minutesEl = document.querySelector('.minutes'),
+secondsEl = document.querySelector('.seconds'),
+ampmEl = document.querySelector('.ampm');
+
+const clock = setInterval(() => {
+    const now = new Date();
+    const getHours = now.getHours() % 12;
+    const getMinutes = now.getMinutes();
+    const getSeconds = now.getSeconds();
+
+    hoursEl.innerHTML = addZero(getHours);
+    minutesEl.innerHTML = addZero(getMinutes)
+    secondsEl.innerHTML = addZero(getSeconds)
+    ampmEl.innerHTML = getAMPM(getHours);
+
+    if(getHours >= 12) {
+        return `PM`;
+    }
+    else {
+        return `AM`
+    }
+})
+
+/* This function add zero 0 to minutes,hour,seconds whenever it's less than 10 to form
+this pattern 00,01,02,03,04,05.....10*/
+function addZero(zero){
+    if (zero < 10) {
+        return `0${zero}`
+    }
+    else {
+        return zero;
+    }
+}
+
+/* function to change it to AM and PM */
+function getAMPM(hour){
+    if(hour >= 12) {
+        return `pm`;
+    }
+    else {
+        return `am`
+    }
+}
