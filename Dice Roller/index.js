a337009f796c2c8d905e990a6b6e8bb668a7db88
Ejay Gabriel <eebudonihian@gmail.com>
2025-08-08 04:08:35 -0500

    I added the functionalities and a readMe file of th dice roller

--- Dice Roller/index.js
+++ Dice Roller/index.js
@@ -1,2 +1,23 @@
 function rollDice() {
+    // define the elements
+    const numOfDice = document.getElementById("numOfDice").value; // get the value
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+
+    // create two empty array to store the values of the dice rolls and images
+    const values = [];
+    const images = [];
+
+    /* create a for loop that will loop base on the dice number selected and then generate a random
+    number between 1 - 6 for each dice we're rolling: if we row 3 dices it should generate 3 random
+     numbers.... like this */
+
+     for (let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1; // random num from 1 - 6;
+        values.push(value);
+        images.push(`<img src="images/dice${value}.png" alt="Dice${value}" width="150px" class="m-5">`);
+     }
+
+     diceResult.innerHTML = `Dice: ${values.join(',')}`;
+     diceImages.innerHTML = images.join(" ");
 }
---
+++ Dice Roller/readMe.md
@@ -0,0 +1,16 @@
+# Dice Roller
+
+This is a simple, interactive dice roller application built with HTML, Tailwind CSS, and JavaScript.
+
+## Features
+
+- Roll one or more dice at a time.
+- View the result of each roll.
+- See a visual representation of the rolled dice.
+
+## How to Use
+
+1. Open the `index.html` file in your web browser.
+2. Enter the number of dice you want to roll in the input field.
+3. Click the "Roll Dice" button.
+4. The results and images of the dice will be displayed on the screen.
