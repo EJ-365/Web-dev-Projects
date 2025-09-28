ebaff6cf5307ba9f8e58238954cf4fb6c3623c21
Ejay Gabriel <eebudonihian@gmail.com>
2025-09-28 02:06:00 -0500

    Finished the mark-up and set up some of the variable declaration in the index.js file

---
+++ Rock-Paper-Scissors/index.js
@@ -0,0 +1,14 @@
+/* This program demonstrate rock paper and scissors game */
+const choices = ["rock", "paper", "scissors"];
+
+// player and computer display
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+
+// result display 
+const resultDisplay = document.getElementById("resultDisplay");
+
+// playGame() function
+function playGame() {
+    alert("App In progress, coming soon!!!")
+}
